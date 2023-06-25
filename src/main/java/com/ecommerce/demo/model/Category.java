b70e1e7b5fce11e1be504fbf6eb029216c75513a
nikldev0 <nikhil.kanukuntla@protonmail.com>
2023-06-25 21:54:22 +0100

    Create category entity class

---
+++ src/main/java/com/ecommerce/demo/model/Category.java
@@ -0,0 +1,33 @@
+package com.ecommerce.demo.model;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import jakarta.validation.constraints.NotBlank;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity //this annotation means objects of this class will be persisted to a db
+@Table(name = "categories")
+@NoArgsConstructor
+@Getter
+@Setter
+public class Category {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @Column(name = "category_name")
+    private @NotBlank String categoryName;
+    private @NotBlank String description;
+
+    @Column(name = "image_url")
+    private @NotBlank String imageUrl;
+
+
+}
